a6d7f94e441b06563a36b7d73e888f3eaa27fdde
hikkisan127 <109756167+hikkisan127@users.noreply.github.com>
2022-07-30 14:57:23 -0300

    Update t.py

--- t.py
+++ t.py
@@ -3,7 +3,6 @@ import streamlit as st
 import time
 import numpy as np
 from PIL import Image
-import plotly.graph_objects as go
 import pandas as pd
 import cufflinks as cf
 import plotly.graph_objects as go
